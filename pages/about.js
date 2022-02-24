1e3decc0f58ddc0ff16faf82ba68a24a8ba2f3ff
Ibrahim Shittu <ibshittu01@gmail.com>
2022-02-24 21:25:00 +0100

    first commit

---
+++ pages/about.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+
+const about = () => {
+  return (
+      
+    <div>
+        <Head>
+            <title>Web Dev news about</title>
+            <meta name="keywords" content='web development for beginners'/>
+        </Head>
+      about
+    </div>
+  )
+}
+
+export default about
