8c838b64e8a1453d37abb1a753452b279ba1425c
Ibrahim Shittu <ibshittu01@gmail.com>
2022-02-25 09:52:48 +0100

    dynamic meta head

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,4 +1,5 @@
 import {useRouter} from 'next/router'
+import {server} from '../../../config/index'
 import Link from 'next/link'
 
 const article = ({article}) => {
@@ -41,5 +42,34 @@ export const getStaticPaths = async () => {
         
     }
 }
+
+
+// export const getStaticProps = async (context) => {
+//     const res = await fetch(`${server}/api/articles/${context.params.id}`)
+
+//     const article = await res.json()
+
+//     return {
+//         props: {
+//             article
+//         }
+//     }
+// }
+
+// export const getStaticPaths = async () => {
+//     const res = await fetch(`${server}/api/articles`)
+
+//     const articles = await res.json()
+
+//     const ids = articles.map(article => article.id)
+//     const paths = ids.map(id => ({params: {id: id.toString()}}))
+
+//     return {
+   
+//             paths, 
+//             fallback: false,
+        
+//     }
+// }
 
 export default article
